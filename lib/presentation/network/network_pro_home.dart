545fe2969b6e1b0220f15ffb83cd5585f22bd804
Thixinnovation <luminatbusinessgroup@gmail.com>
2026-06-09 14:05:07 +0300

    Update network_pro_home.dart

--- lib/presentation/network/network_pro_home.dart
+++ lib/presentation/network/network_pro_home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';  // ← AJOUTER CET IMPORT
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:thix_id/auth/auth_controller.dart';
